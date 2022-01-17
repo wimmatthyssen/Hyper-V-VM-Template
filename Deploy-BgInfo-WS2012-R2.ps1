a5617047a34e24f6b9d5224ff6d10236aa1578bf
Wim Matthyssen <62010638+wimmatthyssen@users.noreply.github.com>
2022-01-17 14:39:07 +0100

    Update Deploy-BgInfo-WS2012-R2.ps1

--- Deploy-BgInfo-WS2012-R2.ps1
+++ Deploy-BgInfo-WS2012-R2.ps1
@@ -5,16 +5,19 @@ A script used to download, install and configure the latest BgInfo version on a 
 
 .DESCRIPTION
 
-A script used to download, install and configure the latest BgInfo version (v4.28) on a Windows Server 2012 R2. The BgInfo folder will be created on the C: drive if the folder does not already exist. 
-Then the latest BgInfo.zip file will be downloaded and extracted in the BgInfo folder. The LogonBgi.zip file which holds the preferred settings will also be downloaded and extracted to the BgInfo folder. 
-After extraction both .zip files will be deleted. A registry key (regkey) to AutoStart the BgInfo tool in combination with the logon.bgi config file will be created. At the end of the script BgInfo will 
-be started for the first time and the PowerShell window will be closed.
+A script used to download, install and configure the latest BgInfo version (v4.28) on a Windows Server 2012 R2. 
+The BgInfo folder will be created on the C: drive if the folder does not already exist. 
+Then the latest BgInfo.zip file will be downloaded and extracted in the BgInfo folder. 
+The LogonBgi.zip file which holds the preferred settings will also be downloaded and extracted to the BgInfo folder. 
+After extraction both .zip files will be deleted. 
+A registry key (regkey) to AutoStart the BgInfo tool in combination with the logon.bgi config file will be created. 
+At the end of the script BgInfo will be started for the first time and the PowerShell window will be closed.
 
 .NOTES
 
 File Name:      Deploy-BgInfo-WS2012-R2.ps1
 Created:        17/09/2018
-Last modified:  26/07/2021
+Last modified:  16/01/2022
 Author:         Wim Matthyssen
 PowerShell:     4.0 or above 
 Requires:       -RunAsAdministrator
@@ -29,7 +32,7 @@ Disclaimer:     This script is provided "As Is" with no warranties.
 
 .LINK
 
-https://tinyurl.com/yxpbq8la
+https://wmatthyssen.com/2019/09/11/powershell-bginfo-automation-script-for-windows-server-2012-r2/
 #>
 
 ## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -40,9 +43,9 @@ $bgInfoFolder = "C:\BgInfo"
 $bgInfoFolderContent = $bgInfoFolder + "\*"
 $itemType = "Directory"
 $bgInfoUrl = "https://download.sysinternals.com/files/BGInfo.zip"
-$logonBgiUrl = "https://tinyurl.com/yxlxbgun"
 $bgInfoZip = "C:\BgInfo\BgInfo.zip"
 $bgInfoEula = "C:\BgInfo\Eula.txt"
+$logonBgiUrl = "https://tinyurl.com/yxlxbgun"
 $logonBgiZip = "C:\BgInfo\LogonBgi.zip"
 $bgInfoRegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
 $bgInfoRegkey = "BgInfo"
@@ -56,24 +59,29 @@ $writeEmptyLine = "`n"
 
 ## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Download started
+## Start script execution
 
-Write-Host ($writeEmptyLine + "# BgInfo download started")`
+Write-Host ($writeEmptyLine + "# BgInfo deployment script started")`
 -foregroundcolor $foregroundColor1 $writeEmptyLine 
-
+ 
 ## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 
 ## Create BgInfo folder on C: if it not exists, else delete it's content
 
-If (!(Test-Path -Path $bgInfoFolder)){New-Item -ItemType $itemType -Force -Path $bgInfoFolder
-    Write-Host ($writeEmptyLine + "# BgInfo folder created")`
-    -foregroundcolor $foregroundColor2 $writeEmptyLine
- }Else{Write-Host ($writeEmptyLine + "# BgInfo folder already exists")`
-    -foregroundcolor $foregroundColor1 $writeEmptyLine
-    Remove-Item $bgInfoFolderContent -Force -Recurse -ErrorAction SilentlyContinue
-    Write-Host ($writeEmptyLine + "# Content existing BgInfo folder deleted")`
-    -foregroundcolor $foregroundColor1 $writeEmptyLine}
+If (!(Test-Path -Path $bgInfoFolder))
+{
+       New-Item -ItemType $itemType -Force -Path $bgInfoFolder
+       Write-Host ($writeEmptyLine + "# BgInfo folder created")`
+       -foregroundcolor $foregroundColor2 $writeEmptyLine
+}
+Else
+{
+       Write-Host ($writeEmptyLine + "# BgInfo folder already exists")`
+       -foregroundcolor $foregroundColor2 $writeEmptyLine
+       Remove-Item $bgInfoFolderContent -Force -Recurse -ErrorAction SilentlyContinue
+       Write-Host ($writeEmptyLine + "# Content existing BgInfo folder deleted")`
+       -foregroundcolor $foregroundColor2 $writeEmptyLine
+}
 
 ## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -85,6 +93,7 @@ Start-BitsTransfer -Source $bgInfoUrl -Destination $bgInfoZip
 [System.IO.Compression.ZipFile]::ExtractToDirectory($bgInfoZip, $bgInfoFolder)
 Remove-Item $bgInfoZip
 Remove-Item $bgInfoEula
+
 Write-Host ($writeEmptyLine + "# bginfo.exe available")`
 -foregroundcolor $foregroundColor2 $writeEmptyLine
 
@@ -96,6 +105,7 @@ Invoke-WebRequest -Uri $logonBgiUrl -OutFile $logonBgiZip
 [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
 [System.IO.Compression.ZipFile]::ExtractToDirectory($logonBgiZip, $bgInfoFolder)
 Remove-Item $logonBgiZip
+
 Write-Host ($writeEmptyLine + "# logon.bgi available")`
 -foregroundcolor $foregroundColor2 $writeEmptyLine
 
@@ -103,19 +113,26 @@ Write-Host ($writeEmptyLine + "# logon.bgi available")`
 
 ## Create BgInfo Registry Key to AutoStart
 
-If ($regKeyExists -eq $True){Write-Host ($writeEmptyLine + "# BgInfo regkey exists, script wil go on")`
--foregroundcolor $foregroundColor1 $writeEmptyLine
-}Else{
-New-ItemProperty -Path $bgInfoRegPath -Name $bgInfoRegkey -PropertyType $bgInfoRegType -Value $bgInfoRegkeyValue
-Write-Host ($writeEmptyLine + "# BgInfo regkey added")`
--foregroundcolor $foregroundColor2 $writeEmptyLine}
+If ($regKeyExists -eq $True)
+{
+   Write-Host ($writeEmptyLine + "# BgInfo regkey exists, script wil go on")`
+   -foregroundcolor $foregroundColor1 $writeEmptyLine
+}
+Else
+{
+   New-ItemProperty -Path $bgInfoRegPath -Name $bgInfoRegkey -PropertyType $bgInfoRegType -Value $bgInfoRegkeyValue
+
+   Write-Host ($writeEmptyLine + "# BgInfo regkey added")`
+   -foregroundcolor $foregroundColor2 $writeEmptyLine
+}
 
 ## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ## Run BgInfo
 
 C:\BgInfo\Bginfo.exe C:\BgInfo\logon.bgi /timer:0 /nolicprompt
-Write-Host ($writeEmptyLine + "# BgInfo has run")`
+
+Write-Host ($writeEmptyLine + "# BgInfo has ran for the first time")`
 -foregroundcolor $foregroundColor2 $writeEmptyLine
 
 ## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
